4eaece31df3fb1f447364abbd4402dd14b898a9f
LeonID <lmatskevich79@gmail.com>
2023-03-20 14:59:15 +0300

    feat: add display text from localstorage

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import axios from 'axios';
 import styles from './Search.module.scss';
 import SearchIcon from '@mui/icons-material/Search';
 
-const Search = () => {
-  return (
-    <>
-      <h1>search</h1>
+interface State {
+  searchValue: string;
+}
+
+export default class Search extends Component<object, State> {
+  state: State = {
+    searchValue: localStorage.getItem('searchValue') as string
+  };
+
+  render(): JSX.Element {
+    return (
       <div className={styles.form}>
         <form className={styles.formImput}>
           <SearchIcon className={styles.searchIcon} />
@@ -14,30 +21,15 @@ const Search = () => {
             className={styles.searchImput}
             placeholder="Search input"
             type="search"
-            name="q"
-            id=""
-            // value="{username}"
+            value={this.state.searchValue}
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
               const valueEvent: string | null = event.target.value;
-              localStorage.setItem('inputValue', valueEvent);
-              const getInputValue: string | null =
-                localStorage.getItem('inputValue');
-
-              // console.log(valueEvent);
-              // console.log(username);
-              // console.log(event);
-              return getInputValue;
+              this.setState({ searchValue: event.target.value });
+              localStorage.setItem('searchValue', valueEvent);
             }}
           />
         </form>
       </div>
-      {/* <div className={styles.countries}>
-        {countries.map((country: any, index: number) => {
-          return <CountryItem country={country} key={index} />;
-        })}
-      </div> */}
-    </>
-  );
-};
-
-export default Search;
+    );
+  }
+}
