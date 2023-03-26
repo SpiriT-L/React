effb8b7d7817f5999700a9328b67524dc3462d22
LeonID <lmatskevich79@gmail.com>
2023-03-26 18:48:31 +0300

    feat: add editable components to select

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,3 +1,16 @@
 export interface SearchInput {
   value: string;
 }
+
+export interface Form {
+  value: string;
+  inputName: string;
+  inputSurname: string;
+  e?: number;
+  inputDate: string | number;
+  event?: string | null | undefined | unknown;
+  inputGender?: string;
+  inputSelect: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  showData?: string | undefined | null | any;
+}
